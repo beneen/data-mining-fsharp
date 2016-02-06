cd8302bad6dd04ed383c3e6353147e17779ef2e7
Beneen Tahir <beneen.tahir@gmail.com>
2016-02-06 16:11:16 +0000

    First attempt at KNN

--- DataMiningInFSharp/DataMining/KNN.fs
+++ DataMiningInFSharp/DataMining/KNN.fs
@@ -10,5 +10,49 @@
 // the classification is whether the target stock has goes up or down after the next day.
 
 // to begin with pick 10 stocks and choose one to be the target stock 
+open DataMiningInFSharp.DataMining.Configuration
+open DataMiningInFSharp.DataMining.StockLoader
+open DataMiningInFSharp.DataMining.DataSetConverter
+open Accord.MachineLearning
+open Accord.Math
+open Accord.Statistics.Analysis
 
+open System.IO
+open NUnit.Framework
+open FsUnit
 
+let doKnn (dataSet: DataSet) =
+    let crossValidation = CrossValidation<KNearestNeighbors>(dataSet.TrainingSet.Length)
+    let fitting (k:int) (indicesTrain: int[]) (indicesValidation: int[]) = 
+        let trainingInputs = dataSet.TrainingSet.Submatrix(indicesTrain)
+        let trainingOutputs = dataSet.Classifications.Submatrix(indicesTrain)
+
+        let validationInputs = dataSet.TrainingSet.Submatrix(indicesValidation)
+        let validationOutputs = dataSet.Classifications.Submatrix(indicesValidation)
+
+        let knn = KNearestNeighbors(11, trainingInputs, trainingOutputs)
+
+        let trainingPredictions = trainingInputs |> Seq.map (fun input -> knn.Compute input) |> Seq.toArray
+        let trainingConfusion = ConfusionMatrix(trainingPredictions, trainingOutputs)
+
+        let validationPredictions = validationInputs |> Seq.map (fun input -> knn.Compute input) |> Seq.toArray
+        let validationConfusion = ConfusionMatrix(validationPredictions, validationOutputs)
+        
+        CrossValidationValues<KNearestNeighbors>(knn, trainingConfusion.Accuracy, validationConfusion.Accuracy)
+
+    crossValidation.Fitting <- CrossValidationFittingFunction(fitting)
+
+    let result = crossValidation.Compute()
+    printfn "Average training accuracy: %f" result.Training.Mean
+    printfn "Average validation accuracy: %f" result.Validation.Mean
+    result.Validation.Mean
+    
+[<TestFixture>] 
+type ``given stock files that have been downloaded`` ()=
+    let targetStock = loadFile "EBAY"
+    let otherStocks = [| "MSFT"; "JPM"; "GS"; "GOOGL" |] |> Seq.map loadFile
+    //let otherStocks = DirectoryInfo(Configuration.stockPath).GetFiles() |> Seq.map (fun file -> file.Name.Substring(0, file.Name.Length - 4)) |> Seq.map loadFile
+    let dataSet = convertToDataSet targetStock otherStocks
+    [<Test>] member test.
+     ``when I convert to a data set`` ()=
+            doKnn dataSet |> should equal 0.5
